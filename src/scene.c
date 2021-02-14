8a7a603d3a4cf77fff99fa85d5138e9b81ae3a69
Federico De Felici <fdf8210@gmail.com>
2021-02-14 13:07:32 +0100

    project creation

---
+++ src/scene.c
@@ -0,0 +1,13 @@
+#include "scene.h"
+
+scene* scene_create(int screen_width, int screen_height, SDL_Renderer* r) {
+
+}
+
+void scene_update(scene* s, float delta_time) {
+
+}
+
+void scene_destroy(scene* s) {
+    
+}
